9479d642e4e23cd96476dbdc6b9647e5f4ca2b7b
Leonardo Henrique de Camargo <camargo.leonardo@tecpuc.com.br>
2020-08-05 01:47:57 -0300

    fim dia 2 next level week

--- leo/server/src/server.ts
+++ leo/server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
-
-app.get('/' , (request, response) => {
-    return response.json({ message: 'Hello World'});
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
