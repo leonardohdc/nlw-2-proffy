5206737eb96e4de650fafd3b9c1346f44d953cc0
Leonardo Henrique de Camargo <camargo.leonardo@tecpuc.com.br>
2020-08-08 19:04:05 -0300

    testando um bug no web

--- leo/web/src/pages/TeacherForm/index.tsx
+++ leo/web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css';
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrivel que você quer dar aulas"/>
+            <PageHeader 
+                title="Que incrivel que você quer dar aulas"
+                description="O primeiro passo é preenche esse formulário de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+                    <Textarea name="bio" label="Biografia" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    
+                    <Select name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importantes" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
